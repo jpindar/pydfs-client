cd4a3ca537372354f84974a3461bc970955e7289
Jeanne Pindar <jpindar@jpindar.com>
2019-11-27 23:46:23 -0500

    added various filters and forces

--- dfs1.py
+++ dfs1.py
@@ -16,13 +16,46 @@ number_of_lineups = 10
 filename = os.getcwd() + os.sep + output_filename   # could use "\\"
 print("writing to " + output_filename)
 newFile = open(output_filename, 'w')
+newFile.write('No lions \n')
+newFile.write('No players below 6.0 points\n')
+newFile.write('add Tarik Cohen\n')
+newFile.write('remove Devonta Freeman (just back from foot injury)')
+newFile.write('add Drew brees');
+
 
 optimizer = get_optimizer(Site.FANDUEL, Sport.FOOTBALL)
 optimizer.load_players_from_csv(lineup_filename)
 
-exporter = CSVLineupExporter(optimizer.optimize(n=number_of_lineups))
-exporter.export(csv_filename)
 
+# Stafford is out this week, let's not play any Lions
+# player = optimizer.get_player_by_name('Matthew Stafford')
+# optimizer.remove_player(player)
+lions = filter(lambda p: p.team == 'DET', optimizer.players)
+for player in lions:
+    optimizer.remove_player(player)
+
+
+
+# remove low ranked players (who might not even start!)
+minimum_rank = 6.0
+for player in optimizer.players:
+    if player.fppg < minimum_rank:
+        optimizer.remove_player(player)
+
+
+# Devonta Freeman  is just back from a foot injury
+player = optimizer.get_player_by_name('Devonta Freeman')
+optimizer.remove_player(player)
+
+
+# recommended by Sportsline
+player = optimizer.get_player_by_name('Tarik Cohen')
+optimizer.add_player_to_lineup(player)
+  
+  
+# just because
+player = optimizer.get_player_by_name('Drew Brees')
+optimizer.add_player_to_lineup(player)
 
 for lineup in optimizer.optimize(n=number_of_lineups):
     newFile.write("\n***********\n")
@@ -31,6 +64,9 @@ for lineup in optimizer.optimize(n=number_of_lineups):
 
 newFile.close()
 
+
+# exporter = CSVLineupExporter(optimizer.optimize(n=number_of_lineups))
+# exporter.export(csv_filename)
 
 
 
