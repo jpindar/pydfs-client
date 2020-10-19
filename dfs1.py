5b9cb4f199bb98ebbe38d802e39014ba1140add1
Jeanne Pindar <jpindar@jpindar.com>
2020-10-19 15:21:50 -0400

    removed defenses from the 'remove low ranked players' code because all the defenses are low ranked

--- dfs1.py
+++ dfs1.py
@@ -8,8 +8,9 @@ This uses pydfs to create lineups
 import os
 from pydfs_lineup_optimizer import get_optimizer, Site, Sport, CSVLineupExporter
 
+# lineup_filename = "FanDuel-NFL-2020-10-18-50949-players-list-friends.csv"
+lineup_filename = "FanDuel-NFL-2020-10-18-50946-players-list-special.csv"
 
-lineup_filename = "FanDuel-NFL-2019-11-28-40929-players-list.csv"
 output_filename = "lineups1"
 number_of_lineups = 10
 note1 = ""
@@ -27,17 +28,19 @@ print('loading players')
 optimizer.load_players_from_csv(lineup_pathname)
 print('optimizing...')
 
+# remove low ranked players
+newFile.write('removing players below minimum rank of ' + str(minimum_rank) + '\n')
+for player in optimizer.players:
+    if player.fppg < minimum_rank:
+        if player.positions[0] != 'D':
+            optimizer.remove_player(player)
+
+
 teamName = 'DET'
 thisTeam = filter(lambda p: p.team == teamName, optimizer.players)
 for player in thisTeam:
     optimizer.remove_player(player)
 
-
-
-# remove low ranked players
-for player in optimizer.players:
-    if player.fppg < minimum_rank:
-        optimizer.remove_player(player)
 
 playerName = 'Devonta Freeman'
 player = optimizer.get_player_by_name(playerName)
